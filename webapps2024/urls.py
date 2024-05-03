9f61742e01cfb245d829a34dd75bd7ed78751eba
Bolademola <Bolademola01@gmail.com>
2024-05-03 09:38:59 -0700

    first commit

---
+++ webapps2024/urls.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('register.urls')),
+    path('transactions/', include('register.urls')),
+    path('history/', include('register.urls')),
+    path('profile/', include('register.urls')),
+    path('register/', include('register.urls')),
+    path('logout/', include('register.urls')),
+    path('login/', include('register.urls')),
+
+    path('deposit_money/', include('register.urls')),
+    path('withdraw_money/', include('register.urls')),
+    path('change-currency/', include('register.urls')),
+
+    path('create_payment_requests/', include('register.urls')),
+    path('view_payment_requests/', include('register.urls')),
+    path('accept_payment_request/<int:pk>/', include('register.urls')),
+    path('reject_payment_request/<int:pk>/', include('register.urls')),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
